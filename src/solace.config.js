8447f5b07e0f04038c51186ea9952b212f1a4c1f
Andrew Roberts <andrew.jacob.roberts@gmail.com>
2021-06-21 09:10:03 -0400

    update

---
+++ src/solace.config.js
@@ -0,0 +1,7 @@
+// GCP
+export const solaceConfig = {
+  SOLACE_HOST_URL: "tcps://mr-lkwaj3bdch0.messaging.solace.cloud:55443",
+  SOLACE_MESSAGE_VPN: "gcp-us-central",
+  SOLACE_USERNAME: "solace-cloud-client",
+  SOLACE_PASSWORD: "gn0ka1hmr2u1aaok1s2iaa4acd",
+};
